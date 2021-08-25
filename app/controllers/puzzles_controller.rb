7213ca39d199fbd4feeb1b21153a31d240e2e2f0
Reid <rlj0713@gmail.com>
2021-08-25 17:52:08 -0400

    removed unused code from PuzzlesController - index

--- app/controllers/puzzles_controller.rb
+++ app/controllers/puzzles_controller.rb
@@ -3,8 +3,6 @@ class PuzzlesController < ApplicationController
     def index
         puzzles = Puzzle.all
         render json: PuzzleSerializer.new(puzzles, { include: [:difficulty, :approved] } )
-
-        # render json: puzzles.to_json(except: [:created_at, :updated_at, :difficulty_id], include: {difficulty: {only: [:name, :level]}})
     end
 
     def show
